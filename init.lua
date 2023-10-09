fa37853dfbecb7a9311b9c1e8daa342f2752a857
Wang HengXing <35129760@qq.com>
2023-10-09 13:54:50 +0800

    chore: 更细配置.

--- init.lua
+++ init.lua
@@ -149,3 +149,19 @@ require("bigfile").setup {
 	},
 }
 
+
+
+local gitlab = require("gitlab")
+vim.keymap.set("n", "<leader>glr", gitlab.review)
+vim.keymap.set("n", "<leader>gls", gitlab.summary)
+vim.keymap.set("n", "<leader>glA", gitlab.approve)
+vim.keymap.set("n", "<leader>glR", gitlab.revoke)
+vim.keymap.set("n", "<leader>glc", gitlab.create_comment)
+vim.keymap.set("n", "<leader>gln", gitlab.create_note)
+vim.keymap.set("n", "<leader>gld", gitlab.toggle_discussions)
+vim.keymap.set("n", "<leader>glaa", gitlab.add_assignee)
+vim.keymap.set("n", "<leader>glad", gitlab.delete_assignee)
+vim.keymap.set("n", "<leader>glra", gitlab.add_reviewer)
+vim.keymap.set("n", "<leader>glrd", gitlab.delete_reviewer)
+vim.keymap.set("n", "<leader>glp", gitlab.pipeline)
+vim.keymap.set("n", "<leader>glo", gitlab.open_in_browser)
