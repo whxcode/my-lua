5b1ef20a6ddbaf39ea723f6b2009ceb2545e1797
Wang HengXing <35129760@qq.com>
2024-03-18 10:29:15 +0800

    feature: add history.

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -121,4 +121,21 @@ return {
       end,
     },
   },
+
+  {
+    "telescope.nvim",
+    dependencies = {
+      "dawsers/telescope-file-history.nvim",
+      config = function()
+        -- These are the default values
+        require("file_history").setup({
+          -- This is the location where it will create your file history repository
+          backup_dir = "~/.file-history-git",
+          -- command line to execute git
+          git_cmd = "git",
+        })
+        require("telescope").load_extension("file_history")
+      end,
+    },
+  },
 }
